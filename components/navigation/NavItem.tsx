3e33bf3951293134dac006b15cd55547382fbea5
DuskWarden <pawar96sameer@gmail.com>
2026-04-22 20:16:03 +0530

    Port isOpen prop from master for dropdown rotations and revert docsearch import

--- components/navigation/NavItem.tsx
+++ components/navigation/NavItem.tsx
@@ -12,6 +12,7 @@ interface NavItemProps {
   onMouseEnter?: () => void;
   hasDropdown?: boolean;
   className?: string;
+  isOpen?: boolean;
 }
 
 /**
@@ -31,7 +32,8 @@ export default function NavItem({
   onClick = () => {},
   onMouseEnter = () => {},
   hasDropdown = false,
-  className = ''
+  className = '',
+  isOpen = false
 }: NavItemProps) {
   const router = useRouter();
 
@@ -72,7 +74,15 @@ export default function NavItem({
         <span className='border-b border-transparent dark:hover:border-white dark:text-dark-text dark:hover:text-dark-heading hover:border-black'>
           {text}
         </span>
-        {hasDropdown && <NavItemDropdown />}
+        {hasDropdown && (
+          <span
+            className={`inline-flex size-5 items-center justify-center transition-transform duration-200 ${
+              isOpen ? 'rotate-180' : 'rotate-0'
+            }`}
+          >
+            <NavItemDropdown />
+          </span>
+        )}
       </Link>
     );
   }
@@ -80,7 +90,15 @@ export default function NavItem({
   return (
     <button type='button' {...attrs} className={`${attrs.className} text-gray-900`}>
       <span>{text}</span>
-      {hasDropdown && <NavItemDropdown />}
+      {hasDropdown && (
+        <span
+          className={`inline-flex size-5 items-center justify-center transition-transform duration-200 ${
+            isOpen ? 'rotate-180' : 'rotate-0'
+          }`}
+        >
+          <NavItemDropdown />
+        </span>
+      )}
     </button>
   );
 }
